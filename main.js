4b67e74498e684ebec8aa80b7e564852a8238424
Ieva Urbaite <ieva@greyepk.com>
2019-10-15 22:47:49 -0400

    created handleStateForm function

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-
+// Global Variable
+const breweries = [];
 
 function populateStates(){
     const arr =[]
@@ -12,8 +13,32 @@ function createSelection(state){
     return `<option value="${state.name.toLowerCase().split(" ").join("_")}">${state.name}</option>`;
 }
 
+function handleStateForm(){
+    $('#state-form').submit(event =>{
+        event.preventDefault();
+        const chosenState = $(`#state-list option:selected`).val();
+    
+        for (let i = 1; i < 20; i++){
+            const url = `https://api.openbrewerydb.org/breweries?by_state=${chosenState}&page=${i}&per_page=50`;
+            fetch(url).then(response => response.json()).then(responseJson => {
+                responseJson.forEach(brewery => {
+                    if (responseJson.length === 0){
+                        i = 20;
+                    } else{
+                        breweries.push(brewery);
+                    }
+            });
+        });
+        console.log(breweries);
+        }
+
+    });
+    
+}
+
 
 
 
 
 $(populateStates());
+$(handleStateForm());
